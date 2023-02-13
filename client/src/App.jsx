afdd69aab6c5d07d8e9a93b558861b54ae7cfaa4
ikram3031 <mdikram.3031@gmail.com>
2023-02-14 01:28:43 +0600

    folder structure added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Route, redirect, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Dashboard/Login';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Category from './pages/Category';
+import SingleProduct from './pages/SingleProduct';
+import Layout from './components/Layout';
 
 const App = () => {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} /> 
+        <Route path='/' element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/product" element={<SingleProduct />} />
+        </Route>
+        <Route path="/dashboard" element={<Dashboard />} />
+
       </Routes>
     </BrowserRouter>
   )

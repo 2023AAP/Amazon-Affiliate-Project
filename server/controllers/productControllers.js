4f11166d9ccdafaa25ce476e212a03e71e5e7c36
Fahad Hossain <fahadshakiblst@gmail.com>
2023-02-20 23:13:20 +0600

    Merge Conflicts Solved

--- server/controllers/productControllers.js
+++ server/controllers/productControllers.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 const fs = require('fs')
 const path = require('path')
-=======
->>>>>>> 6ac163600d721a63a2111ebae500070e996e7391
 const { StatusCodes } = require('http-status-codes')
 const multer = require('multer')
 const Product = require('../models/Product')
@@ -10,7 +7,6 @@ const AppError = require('../utils/appError')
 const catchAsync = require('../utils/catchAsync')
 
 const storage = multer.diskStorage({
-<<<<<<< HEAD
 
     destination: (req, file, callback) => {
         callback(null, 'img/products')
@@ -37,34 +33,6 @@ const createProduct = catchAsync(async (req, res , next) => {
 
     const file = req.files[0];
     if(file) req.body.image = file.filename;
-=======
-
-    destination: (req, file, callback) => {
-        callback(null, 'img/products')
-    },
-
-    filename: (req, file, callback) => {
-        const extension = file.mimetype.split('/')[1];
-        callback(null, `product-${file.originalname.split('.')[0]}-${Date.now()}.${extension}`)
-    }
-});
-
-
-const fileFilter = (req, file, callback) => {
-
-    if (file.mimetype.startsWith('image')) callback(null, true);
-    else callback(new AppError('Please upload an Image', 400), false);
-}
-
-const upload = multer({ storage, fileFilter });
-
-const uploadAnything = upload.any();
-
-const createProduct = async (req, res) => {
-
-    const file = req.files[0];
-    req.body.image = file.filename;
->>>>>>> 6ac163600d721a63a2111ebae500070e996e7391
     const product = await Product.create(req.body)
 
     res.status(StatusCodes.CREATED).json({
@@ -132,11 +100,6 @@ const getAllProducts = async (req, res) => {
 }
 
 module.exports = {
-<<<<<<< HEAD
     createProduct, getAllProducts, uploadAnything , updateProduct , deleteProduct
 }
 
-=======
-    createProduct, getAllProducts, uploadAnything
-}
->>>>>>> 6ac163600d721a63a2111ebae500070e996e7391

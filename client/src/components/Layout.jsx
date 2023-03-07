2887313c96e944087650a6b594b3ff6612a7814b
fahimtalukdarakash <fahimtalukdarakash@gmail.com>
2023-03-07 13:14:57 +0600

    Navbar updated

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../section/Navbar';
 import Footer from '../section/Footer';
+//import Test from '../section/test';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
--- client/src/section/Navbar.jsx
+++ client/src/section/Navbar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React from 'react';
+import './Navbar.scss';
 const Navbar = () => {
+  const navItems1 = ['Item1', 'Item2', 'Item3', 'Item4', 'Item5', 'Item6'];
+  const navItems2 = ['Item1', 'Item2', 'Item3', 'Item4', 'Item5', 'Item6'];
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className='first_navbar'>
+        <h3>All the men's product</h3>
+      </div>
+      <div>
+        <ul className='second_navbar'>
+          {
+            navItems1.map((navItem)=>{
+              return <li key={navItem}>{navItem}</li>
+            })
+          }
+        </ul>
+      </div>
+      <div>
+        <ul className='second_navbar'>
+          {
+            navItems2.map((navItem)=>{
+              return <li key={navItem}>{navItem}</li>
+            })
+          }
+        </ul>
+      </div>
+    </nav>
   )
 }
-
 export default Navbar
